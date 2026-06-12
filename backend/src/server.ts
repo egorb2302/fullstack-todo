ce6a957c8fe2190d8e17a8b1a833ecc48ac19ec6
href <berendeevegor@gmail.com>
2026-06-13 00:59:05 +0400

    create adding ( POST ) function, learn server req\res logics

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv'
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import path from 'path'
-import fs from 'fs'
+import fs, { read } from 'fs'
 import { ServerTodoType } from './types/types';
 
 dotenv.config()
 const PORT = process.env.PORT || "5000"
+const URL = process.env.URL || "http://localhost:5000"
 const app = express()
 const pathToBD = path.resolve(__dirname, "..", "data", "db.json");
 
@@ -28,6 +29,39 @@ const getTodo = async (id: number): Promise<ServerTodoType | undefined> => {
     const data = await getDataFromBD()
     const current = data.find(t => t.id === id)
     return current
+}
+
+const addTodo = async (todo: Omit<ServerTodoType, 'id'>): Promise<void> => {
+    const uniqueTask = { ...todo }
+    const response = await fetch(`${URL}/todos`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(uniqueTask)
+    })
+    if (!response.ok) throw new Error("Error of adding todo")
+}
+
+const removeTodo = async (id: number): Promise<void> => {
+    const response = await fetch(`${URL}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    if (!response.ok) throw new Error("Error of deleting a todo")
+}
+
+const patchTodo = async (id: number, todo: Partial<ServerTodoType>): Promise<void> => {
+    const response = await fetch(`${URL}/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(todo)
+    })
+    if (!response.ok) throw new Error("Error of patching a task")
 }
 
 app.get('/todos', async (req: Request, res: Response) => {
@@ -52,6 +86,24 @@ app.get(`/todos/:id`, async (req: Request, res: Response) => {
     } catch (err) {
         console.error(err)
         res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+app.post('/todos', async (req: Request, res: Response) => {
+    try {
+        const newTask: ServerTodoType = {
+            id: Date.now(),
+            title: req.body.title,
+            description: req.body.description,
+            isCompleted: req.body.isCompleted
+        }
+        const readed = await fs.promises.readFile(pathToBD, "utf-8")
+        const parsed = JSON.parse(readed)
+        await fs.promises.writeFile(pathToBD, JSON.stringify([ ...parsed, newTask ]), "utf-8")
+        res.status(201).json({ message: "Task adding was successfully", task: newTask})
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ error: "Post todo error" })
     }
 })
 
