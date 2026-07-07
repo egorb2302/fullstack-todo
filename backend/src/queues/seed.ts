549fa1572749f2f73dbb286b34724697b1badca9
href <berendeevegor@gmail.com>
2026-07-07 18:18:26 +0400

    adsdasa

--- backend/src/queues/seed.ts
+++ backend/src/queues/seed.ts
@@ -3,18 +3,22 @@ import { todos, users } from '../db/schema';
 import { reportQueue } from './queue';
 
 export const testQueue = async () => {
-    console.log('📤 Adding test job to queue...');
+    try {
+        console.log('📤 Adding test job to queue...');
     
-    const job = await reportQueue.add('test-job', {
-        totalTasks: await db.$count(db.select().from(todos)),
-        totalUsers: await db.$count(db.select().from(users)),
-        timestamp: `Recieved at ${new Date()}`,
-    });
-    
-    console.log('✅ Job added:', job.id);
-    console.log('📊 Queue stats:', await reportQueue.getJobCounts());
+        const job = await reportQueue.add('test-job', {
+            totalTasks: await db.$count(db.select().from(todos)),
+            totalUsers: await db.$count(db.select().from(users)),
+            timestamp: `Recieved at ${new Date()}`,
+        });
+        
+        console.log('✅ Job added:', job.id);
+        console.log('📊 Queue stats:', await reportQueue.getJobCounts());
 
-    return job
+        return job
+    } catch (er) {
+        console.warn(er)
+    }
 };
 
 testQueue().catch(err => console.error(err));
