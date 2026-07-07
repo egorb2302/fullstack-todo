166b8cc947a3281edad7572590b8e350218370bb
href <berendeevegor@gmail.com>
2026-07-07 17:44:01 +0400

    another another fix

--- backend/drizzle.config.js
+++ backend/drizzle.config.js
@@ -1,22 +1,7 @@
-// import { defineConfig } from 'drizzle-kit';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// export default defineConfig({
-//     schema: './src/db/schema.ts',
-//     out: './src/db/migrations',
-//     dialect: 'postgresql',
-//     dbCredentials: {
-//         url: process.env.DATABASE_URL!,
-//     },
-// });
-
 const { defineConfig } = require('drizzle-kit');
 require('dotenv').config();
 
 module.exports = defineConfig({
-    schema: './src/db/schema.ts',
     out: './src/db/migrations',
     dialect: 'postgresql',
     dbCredentials: {
